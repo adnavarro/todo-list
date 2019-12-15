99200b46c870c98bf3c41abd40b57ca59659e194
adnavarro <adnavarro.16@est.ucab.edu.ve>
2019-12-15 14:00:32 -0400

    Borrados archivos no necesarios

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import user from "./reducers/user";
-import callCenter from "./reducers/callCenter";
 
 export default combineReducers({
-    user,
-    callCenter
+    user
 });
--- src/types.js
+++ src/types.js
@@ -1,4 +1,3 @@
 export const USER_LOGGED_IN = 'USER_LOGGED_IN';
 export const USER_LOGGED_OUT = 'USER_LOGGED_OUT';
-export const CALLCENT_SIGN_UP = 'CALLCENT_SIGN_UP';
 export const USER_LIST = 'USER_LIST';
