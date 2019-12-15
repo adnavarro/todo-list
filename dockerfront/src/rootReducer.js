29519b6ac72fc70312462c2723aa37620239315c
Alfonso David Navarro Medina <32369156+adnavarro@users.noreply.github.com>
2019-12-15 15:18:00 -0400

    Unido back y front

--- dockerfront/src/rootReducer.js
+++ dockerfront/src/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import user from "./reducers/user";
+import callCenter from "./reducers/callCenter";
 
 export default combineReducers({
-    user
+    user,
+    callCenter
 });
